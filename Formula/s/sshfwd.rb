677d150b1b22d8987673e437c1367bc9473f9700
zhiqiang <zhiqiang2033@gmail.com>
2026-03-01 19:49:45 +0800

    ✨ feat: add sshpass and sshfwd formulas and update README
    Add Homebrew formulas for sshpass and sshfwd utilities to enable easy installation.
    Update README with setup instructions and usage examples for the new formulas.

---
+++ Formula/s/sshfwd.rb
@@ -0,0 +1,29 @@
+class Sshfwd < Formula
+  desc "Lightweight Go-based SSH port forwarding tool – replicates ssh -L/ssh -R core features, no system SSH dependency, cross-platform ready-to-use."
+  homepage "https://github.com/chihqiang/sshfwd"
+  url "https://github.com/chihqiang/sshfwd/archive/refs/tags/v0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "0661728dfec651527a63672329968e1777ccc4b4b79626abfdd927b0bf0cbf1d"
+  license "Apache-2.0"
+  head "https://github.com/chihqiang/sshfwd", branch: "main"
+  
+  depends_on "go@1.23" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "."
+    (etc/"sshfwd").install "config.yaml"
+  end
+
+  service do
+    run [opt_bin/"sshfwd", "-c", etc/"sshfwd/config.yaml"]
+    keep_alive true
+    error_log_path var/"log/sshfwd.log"
+    log_path var/"log/sshfwd.log"
+  end
+
+  test do
+    assert_predicate bin/"sshfwd", :exist?
+    assert_predicate bin/"sshfwd", :executable?
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# homebrew
-家常菜式 Homebrew 仓库，常用软件一键装！没啥复杂，装完秒飞，码农效率飙升，老板都夸你快！
+# 🍲 chihqiang/soft - 家常菜式 Homebrew 仓库
+
+常用软件，一键安装！
+没啥复杂，装完即用，码农效率蹭蹭飙升，老板都夸你“真快”！
+
+## 添加仓库
+
+```bash
+brew tap chihqiang/soft
+```
+
+## 查看本仓库的详细信息
+
+```bash
+brew tap-info chihqiang/soft
+```
+
+## 移除本仓库
+
+```bash
+brew untap chihqiang/soft
+```
+
+## 清理缓存
+
+```bash
+brew cleanup chihqiang/soft
+```
+
+## 常见软件
+
+### [sshpass](https://sourceforge.net/projects/sshpass/)
+
+```bash
+brew install chihqiang/soft/sshpass
+```
+
+### [sshfwd](https://github.com/chihqiang/sshfwd)
+
+```bash
+brew install chihqiang/soft/sshfwd
+```
